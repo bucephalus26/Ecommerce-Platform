2037891bf39601845de8ebaa8855ab79e758ab56
Ibraheem khan <ibkhan2026@gmail.com>
2022-11-05 13:44:20 +0000

    grouped routes

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -11,11 +11,11 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <a href="/admin/category/create" class="btn btn-block bg-gradient-info" style="width: 200px" >Add Category</a>
+          <a href="{{route('admin.category.create')}}" class="btn btn-block bg-gradient-info" style="width: 200px" >Add Category</a>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Home</a></li>
             <li class="breadcrumb-item active">Category List</li>
           </ol>
         </div>
@@ -57,10 +57,10 @@
               <td>{{$rs->description}}</td>
               <td>{{$rs->image}}</td>
               <td>{{$rs->status}}</td>
-              <td><a href="/admin/category/edit/{{$rs->id}}" class="btn btn-block btn-info btn-sm">Edit</a></td>
-              <td><a href="/admin/category/destroy/{{$rs->id}}" class="btn btn-block btn-danger btn-sm"
+              <td><a href="{{route('admin.category.edit', ['id'=>$rs->id])}}" class="btn btn-block btn-info btn-sm">Edit</a></td>
+              <td><a href="{{route('admin.category.destroy', ['id'=>$rs->id])}}" class="btn btn-block btn-danger btn-sm"
                     onclick="return confirm('Are you sure you want to delete?')">Delete</a></td>
-              <td><a href="/admin/category/show/{{$rs->id}}" class="btn btn-block btn-success btn-sm">Show</a></td>
+              <td><a href="{{route('admin.category.show', ['id'=>$rs->id])}}" class="btn btn-block btn-success btn-sm">Show</a></td>
             </tr>
             @endforeach
           </tbody>
