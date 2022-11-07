e0606428bcc65d05ac10c3f7c6f58ab88567ef87
Ibraheem khan <ibkhan2026@gmail.com>
2022-11-07 13:26:45 +0000

    admin can now create, editproducts, and view product list

---
+++ resources/views/admin/product/create.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Add Product')
+
+@section('content')    
+
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Add Product</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Home</a></li>
+              <li class="breadcrumb-item active">Add Product</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+   <!-- Main content -->
+   <section class="content">
+   <div class="card card-primary">
+              <div class="card-header">Product Elements</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form role ="form" action="{{route('admin.product.store')}}" method="post" enctype="multipart/form-data">
+                @csrf 
+                <div class="card-body">
+                  
+                  <!-- Main category selection -->
+                  <div class="form-group">
+                    <label>Parent Product</label>
+
+                    <select class="form-control select2" name="category_id" style="width: 100%;">
+                      <!-- Select other categories using getParentsTree function-->
+                      @foreach($data as $rs)
+                        <option value="{{ $rs->id }}">{{ \App\Http\Controllers\AdminPanel\CategoryController::getParentsTree($rs, $rs->title) }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Title</label>
+                    <input type="text" class="form-control" name="title" placeholder="Title">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Keywords</label>
+                    <input type="text" class="form-control" name="keywords" placeholder="Keywords">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Description</label>
+                    <input type="text" class="form-control" name="description" placeholder="Description">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Price</label>
+                    <input type="number" class="form-control" name="price" value="0">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Quantity</label>
+                    <input type="tenumberxt" class="form-control" name="quantity" value="0">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Minimum Quantity</label>
+                    <input type="tenumberxt" class="form-control" name="minquantity" value="0">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Details</label>
+                    <textarea class="form-control" name="detail"></textarea>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputFile">Image</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="image">
+                        <label class="custom-file-label" for="exampleInputFile">Choose Image</label>
+                      </div>
+
+                    </div>
+                  <div class="form-group">
+                        <label>Status</label>
+                        <select class="form-control" name="status">
+                          <option>True</option>
+                          <option>False</option>
+                        </select>
+                      </div>
+
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+              </form>
+            </div>
+
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+@endsection
---
+++ resources/views/admin/product/show.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Show Product: '.$data->title)
+
+@section('content')    
+
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-3">
+            <a href="{{route('admin.product.edit', ['id'=>$data->id])}}" class="btn btn-block bg-gradient-info" style="width: 200px" >Edit</a>
+          </div>
+          <div class="col-sm-3">
+            <a href="{{route('admin.product.destroy', ['id'=>$data->id])}}" onclick="return confirm('Are you sure you want to delete?')" class="btn btn-block bg-gradient-danger" style="width: 200px" >Delete</a>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Home</a></li>
+              <li class="breadcrumb-item active">Show Product</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+   <!-- Main content -->
+   <section class="content">
+   <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Detail Data</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <table class="table table-striped">
+                    <tr>
+                      <th style="width: 200px">Id</th>
+                      <td>{{$data->id}}</td>
+                    </tr>
+                    <tr>
+                      <th>Category</th>
+                      <td>{{$data->category_id}}</td>
+                    </tr>
+                    <tr>
+                      <th>Title</th>
+                      <td>{{$data->title}}</td>
+                    </tr>
+                    <tr>
+                      <th>Keywords</th>
+                      <td>{{$data->keywords}}</td>
+                    </tr>
+                    <tr>
+                      <th>Description</th>
+                      <td>{{$data->description}}</td>
+                    </tr>
+                    <tr>
+                      <th>Price</th>
+                      <td>{{$data->price}}</td>
+                    </tr>
+                    <tr>
+                      <th>Quantiy</th>
+                      <td>{{$data->quantity}}</td>
+                    </tr>
+                    <tr>
+                      <th>Minimum Quantity</th>
+                      <td>{{$data->minquantity}}</td>
+                    </tr>
+                    <tr>
+                      <th>Details</th>
+                      <td>{{$data->detail}}</td>
+                    </tr>
+                    <tr>
+                      <th>Image</th>
+                       <!-- if there is image, load it-->
+                      <td>
+                        @if ($data->image)
+                        <img src="{{Storage::url($data->image)}}" style="height: 100px">
+                        @endif
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>Status</th>
+                      <td>{{$data->status}}</td>
+                    </tr>
+                    <tr>
+                      <th>Create Date</th>
+                      <td>{{$data->created_at}}</td>
+                    </tr>
+                    <tr>
+                      <th>Update Date</th>
+                      <td>{{$data->updated_at}}</td>
+                    </tr>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+@endsection
