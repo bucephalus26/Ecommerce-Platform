cc60835ab319826bdc97ee1c64aea1af96acf86b
ZeyadHigazy <116072302+ZeyadHigazy@users.noreply.github.com>
2022-11-21 14:37:15 +0000

    contact us page final

---
+++ Sports4Us/Contact Page.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<head>
+<html lang="en">
+      <meta name = "viewport" content="with=device-width, initial-scale=1.0">
+         <title>Sports4Us/Contact Us</title>
+         <link rel="Stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="Stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous" />
+         <link rel = "Stylesheet" href="Stylesheets.css">
+
+      </head>
+
+      <body style ="padding-top: 50px">
+
+            <nav class="navbar fixed-top px-sm-5 navbar-expand-sm navbar-dark" style="background-color: black">
+            <a href="index.php" class="navbar-brand mb-0 h1"><img class="d-inline-block align-top"
+                    src="images/logo.png " alt="Logo" width="50" height="50" /></a>
+            <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" class="navbar-toggler"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="index.php" class="nav-link"> Home </a>
+                    </li>
+                    
+                    <li class="nav-item active">
+                        <a href="login.php" class="nav-link"> Login </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a href="signup.php" class="nav-link "> Signup </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a href="Contact Page.php" class="nav-link active"> Contact Us </a>
+                    </li>
+                </ul>
+            </div>
+            <form class="d-flex">
+                <input type="text" class="text form-control me-1" />
+                <button type="submit" class="btn btn-outline-light">Search</button>
+            </form>
+            <li class="nav-item active">
+                <a href="index.php" class="navbar-brand mb-0 h1"><img class="d-inline-block align-top mx3"
+                        src="images/user.png" alt="Logo" width="35" height="35"
+                        style="padding-left: 2px; padding: right 20px;" /></a>
+            </li>
+        </nav>
+<body> 
+    <section class = "Contact">
+        <div class  = "content">
+            <h2> Contact Us</h2>
+            <p> Need to get in touch? Fill out the form with your inquiry.</p>
+        </div>
+        <div class = "container">
+            <div class = "contactInfo">
+            <div class = "box">
+                <div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i>
+                </div>
+                <div class = "text">
+                    <h3>Address</h3>
+                    <p> Aston Street, <br> Birmingham, <br> B4 7ET </p> 
+                </div>
+            </div>
+            <div class = "box">
+                <div class ="icon"><i class="fa fa-phone" aria-hidden="true"></i>
+                </div>
+                <div class = "text">
+                    <h3>Lets Talk</h3>
+                    <p> 01234567890 </p> 
+                </div>
+            </div>
+            <div class = "box">
+                <div class ="icon"><i class="fa fa-envelope" aria-hidden="true"></i>
+                </div>
+                <div class = "text">
+                    <h3>General Support</h3>
+                    <p>Support@Sports4Us.com</p> 
+                </div>
+            </div>
+        </div>
+        <div class = "ContactForm">
+            <form> <h2> Send Message</h2>
+            <div class="InputBox">
+                <input type = "text" name =""  required = "required">
+                <span> Full Name</span>
+            </div>
+            <div class="InputBox">
+                <input type = "text" name =""  required = "required">
+                <span>Email</span>
+            </div>
+            <div class="InputBox">
+                <input type = "text" name =""  required = "required">
+                <span> Phone</span>
+            </div>
+            <div class="InputBox">
+                <textarea required = "required"></textarea>
+                <span> Type Your Message....</span>
+            </div>
+            <div class = "InputBox">
+                <input type = "submit" name="" value = "Send">
+            </div>
+            </form>
+        </div>
+    </div>
+    </section>
+</body>
+
+<footer class="text-center text-lg-start text-muted " style="margin-top: 0px; background-color: black;">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div></div>
+        <div>
+            <a href="https://en-gb.facebook.com/astonuniversity/" class="me-4 text-reset">
+                <i class="fa fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fa fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fa fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fa fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fa fa-github"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <p>useful links</p>
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Home</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Products</a>
+                    </p>
+                    <p>
+                        <a href="login.php" class="text-reset">Account</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Contact Us</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">About Us</a>
+                    </p>
+                </div>
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <a href="#" class="fas fa-gem me-3 text-reset">Subscribe Newsletter</a>
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+                            <div class="subscribe btm-brdr">
+                                <form>
+                                    <input type="email" placeholder="Email" />
+                                    <button type="button" class="fill-btn">Subscribe</button>
+                                </form>
+                    </h6>
+                </div>
+
+            </div>
+    </section>
+
+    <!-- Copyright -->
+    <div class="text-center p-4">
+        © 2022 Copyright:
+        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">sports4us.com</a>
+    </div>
+    <!-- Copyright -->
+       </footer>
+</html>
