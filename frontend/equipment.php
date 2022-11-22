0cd895d93c1748da88dfa032c6ecae8fee217864
ehsan-hus <116072123+ehsan-hus@users.noreply.github.com>
2022-11-22 12:03:36 +0000

    Update equipment.php
    added java script 
    user can view other angles of item using arrows
    can select colour size etc

--- frontend/equipment.php
+++ frontend/equipment.php
@@ -97,8 +97,29 @@
 ">
     <div class="product">
         <div>
+        <button onclick="top3On()" id="top3On" class="arrow"><img src="images/arrowl.png" alt=""style="width:25px;height:25px"></button>
             <img src="images/accessory2.jpeg"style="width:150px; height:150px;"  alt="a1" class="thumbnail">
-            <h4>Description</h4>
+        <button class="arrow" onclick="top3Off()" id="top3Off"><img src="images/arrowr.png" alt=""style="width:25px;height:25px"></button><br>
+            
+        <label>Size :</label>
+        <input type="radio" name="size" value="size"><strong>XS</strong>
+        <input type="radio" name="size" value="size"><strong>S </strong>       
+        <input type="radio" name="size" value="size"><strong>L </strong>       
+        <input type="radio" name="size" value="size"><strong>XL </strong> 
+        
+        <br><label>Colours :</label>
+        <input type="radio" name="color" value="color"><strong>Black</strong>
+        <input type="radio" name="color" value="color"><strong>White </strong>       
+        <input type="radio" name="color" value="color"><strong>Blue </strong>       
+        <input type="radio" name="color" value="color"><strong>Red</strong>
+
+           <div class="counter">
+    <button class="cart_btn" onclick="counterDec()">-</button>
+    <h5 id="counter">0</h5>
+    <button class="cart_btn" onclick="counterInc()">+</button >
+    <button class="cart">Add to cart</button>
+    </div>
+    <h4>Description</h4>
             <br>
             <p>
                 - odour-free
@@ -110,9 +131,30 @@
             <!-- adding prices, descriptions and css for images -->
         </div>
         <div>
-            <img src="images\accessory3.jpg"style="width:150px; height:150px;"  alt="a2" class="thumbnail">
+        <button onclick="top3On()" id="top3On" class="arrow"><img src="images/arrowl.png" alt=""style="width:25px;height:25px"></button>
+            <img src="images/accessory3.jpg"style="width:150px; height:150px;"  alt="a2" class="thumbnail">
+            <button class="arrow" onclick="top3Off()" id="top3Off"><img src="images/arrowr.png" alt=""style="width:25px;height:25px"></button><br>
         </div>
         <div>
+            
+        <label>Size :</label>
+        <input type="radio" name="size" value="size"><strong>XS</strong>
+        <input type="radio" name="size" value="size"><strong>S </strong>       
+        <input type="radio" name="size" value="size"><strong>L </strong>       
+        <input type="radio" name="size" value="size"><strong>XL </strong> 
+        
+        <br><label>Colours :</label>
+        <input type="radio" name="color" value="color"><strong>Black</strong>
+        <input type="radio" name="color" value="color"><strong>White </strong>       
+        <input type="radio" name="color" value="color"><strong>Blue </strong>       
+        <input type="radio" name="color" value="color"><strong>Red</strong>
+
+           <div class="counter">
+    <button class="cart_btn" onclick="counterDec()">-</button>
+    <h5 id="counter">0</h5>
+    <button class="cart_btn" onclick="counterInc()">+</button >
+    <button class="cart">Add to cart</button>
+    </div>
             <h4>Description</h4>
             <br>
             <p>
@@ -187,13 +229,13 @@
                         <br>
 
                         <a href="https://www.instagram.com/ehsanhussain_">
-                            <img src="frontend/images/insta.png" style="width: 30px;; height: 30px;;" alt="IG">
+                            <img src="images/insta.png" style="width: 30px;; height: 30px;;" alt="IG">
                           <!-- adding social media links-->
                             <a href="https://en-gb.facebook.com/astonuniversity/">
-                                <img src="frontend/images/fb.png" style="width: 30px;; height: 30px;;" alt="fb">
+                                <img src="images/fb.png" style="width: 30px;; height: 30px;;" alt="fb">
                                     <!-- adding social media links-->
                                 <a href="https://www.twitter.com/sports4_us">
-                                    <img src="frontend/images/twitter.png" style="width: 30px;; height: 30px;;"
+                                    <img src="images/twitter.png" style="width: 30px;; height: 30px;;"
                                         alt="twitter">
                                        <!-- adding social media links-->
                     </div>
@@ -274,6 +316,92 @@
                 </div>
                 <!-- Copyright -->
             </footer>
+            <script>
+                // variableForCounter
+                  var value= document.getElementById('counter').innerText;
+                  var value= document.getElementById('counter1').innerText;
+                  var value= document.getElementById('counter2').innerText;
+
+
+
+                
+                function top3Off(){
+                  document.getElementById('top3Img').src="images/top3back.webp";
+                  document.getElementById('top3Off').style.display="none";
+                  document.getElementById('top3On').style.display="inline";
+                 }
+                function top3On(){
+                  document.getElementById('top3Img').src="images/top3.jpg";
+                  document.getElementById('top3On').style.display="none";
+                  document.getElementById('top3Off').style.display="inline";
+                 }
+                
+
+
+                 function adidasOff(){
+                  document.getElementById('adidasShirt').src="images/tshirtadidasback.jpg";
+                  document.getElementById('adidasOff').style.display="none";
+                  document.getElementById('adidasOn').style.display="inline";
+                 }
+                 function adidasOn(){
+                  document.getElementById('adidasShirt').src="images/tshirtadidas.jpg";
+                  document.getElementById('adidasOn').style.display="none";
+                  document.getElementById('adidasOff').style.display="inline";
+                 }
+               
+                 
+                 function underShirtOff(){
+                  document.getElementById('underShirtImg').src="images/underarmourtshirtback.webp";
+                  document.getElementById('underShirtOff').style.display="none";
+                  document.getElementById('underShirtOn').style.display="inline";
+                 }
+                 function underShirtOn(){
+                  document.getElementById('underShirtImg').src="images/underarmourtshirt.jpg";
+                  document.getElementById('underShirtOn').style.display="none";
+                  document.getElementById('underShirtOff').style.display="inline";
+                 }
+       
+
+
+
+                 function counterInc(){
+                  document.getElementById('counter').innerText=++value;
+                 }
+                 function counterDec(){
+                    if(value>0){
+                  document.getElementById('counter').innerText=--value;
+                    }
+                    else{
+                  document.getElementById('counter').innerText=value;
+                    }
+                }
+                // second counter
+                function counterInc1(){
+                  document.getElementById('counter1').innerText=++value;
+                 }
+                 function counterDec1(){
+                    if(value>0){
+                  document.getElementById('counter1').innerText=--value;
+                    }
+                    else{
+                  document.getElementById('counter1').innerText=value;
+                    }
+                }
+                // third counter
+                function counterInc2(){
+                  document.getElementById('counter2').innerText=++value;
+                 }
+                 function counterDec2(){
+                    if(value>0){
+                  document.getElementById('counter2').innerText=--value;
+                    }
+                    else{
+                  document.getElementById('counter2').innerText=value;
+                    }
+                }
+                 
+            </script>
+</body>
 </body>
 
 </html>
