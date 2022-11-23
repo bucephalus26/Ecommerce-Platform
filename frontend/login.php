36c778028f63ad9fe8f175a33260c8acb8b49fbe
Shah-K02 <116072342+Shah-K02@users.noreply.github.com>
2022-11-23 19:16:46 +0000

    Update login.php
    Added icons to the footer
    Added comments
    Added styling to the buttons

--- frontend/login.php
+++ frontend/login.php
@@ -9,6 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
             crossorigin="anonymous" />
+        <link href="style.css" rel="stylesheet" />
         <title>Login</title>
     </head>
 
@@ -17,54 +18,61 @@
         <nav class="navbar fixed-top px-sm-5 navbar-expand-sm navbar-dark" style="background-color: black">
             <a href="index.php" class="navbar-brand mb-0 h1"><img class="d-inline-block align-top"
                     src="images/navbar-logo.png" alt="Logo" width="50" height="50" /></a>
-            <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" class="navbar-toggler"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <!-- hamburger menu for small devices -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                data-mdb-target="#navbarToggleExternalContent2" aria-controls="navbarToggleExternalContent1"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars text-dark"></i>
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class=" navbar-nav">
                     <li class="nav-item active">
-                        <a href="index.php" class="nav-link"> Home </a>
+                        <a href="index.php" class="homeL nav-link"> Home </a>
                     </li>
                     <!-- linking to home page section on nav bar -->
                     <li class="nav-item active">
-                        <a href="men.php" class="nav-link "> Men </a>
+                        <a href="men.php" class="menL nav-link"> Men </a>
                     </li>
                     <!-- linking to men page section on nav bar -->
                     <li class="nav-item active">
-                        <a href="women.php" class="nav-link"> Women </a>
+                        <a href="women.php" class="wmenL nav-link"> Women </a>
                     </li>
                     <!-- linking to women page section on nav bar -->
                     <li class="nav-item active">
-                        <a href="equipment.php" class="nav-link"> Equipment </a>
+                        <a href="equipment.php" class="eqipL nav-link"> Equipment </a>
                     </li>
                     <!-- linking to equipment page section on nav bar -->
                     <li class="nav-item active">
-                        <a href="login.php" class="nav-link  active"> Login </a>
+                        <a href="login.php" class="loginL nav-link "> Login </a>
                     </li>
                     <!-- linking to login page section on nav bar -->
                     <li class="nav-item active">
-                        <a href="signup.php" class="nav-link "> Signup </a>
+                        <a href="signup.php" class="signupL nav-link active"> Signup </a>
                     </li>
                     <!-- linking to signup page section on nav bar -->
                 </ul>
             </div>
             <form class="d-flex">
                 <input type="text" class="text form-control me-1" />
-                <button type="submit" class="btn btn-outline-light">Search</button>
+                <button type="submit" class="searchBtn btn btn-outline-light">Search</button>
             </form>
-            <li class="nav-item active">
-                <a href="index.php" class="navbar-brand mb-0 h1"><img class="d-inline-block align-top mx-3"
-                        src="images/user.png" alt="Logo" width="35" height="35"
-                        style="padding-left: 5px; padding: right 20px;" /></a>
-            </li>
-            <li class="nav-item active">
-                <a href="" class="navbar-brand mb-0 h1"><img class="d-inline-block align-top mx-3" src="images/cart.png"
-                        alt="Cart" width="35" height="35" style="padding-left: 5px; padding: right 20px;" /></a>
-            </li>
+            <ul>
+                <li class="nav-item">
+                    <a href="index.php" class="navbar-user mb-0 h1"><img class="d-inline-block align-top mx-3"
+                            src="images/user.png" alt="Logo" width="35" height="35" style="margin-top:15px" /></a>
+                </li>
+            </ul>
+            <ul>
+                <li class="nav-item">
+                    <a href="" class="navbar-basket mb-0 h1"><img class="d-inline-block align-top mx-3"
+                            src="images/cart.png" alt="Cart" width="35" height="35" style="margin-top:15px" /></a>
+                </li>
+            </ul>
         </nav>
 
-        <!-- From here the code for the sign-up form starts -->
+
+        <!-- From here the code for the login form starts -->
         <div class="mt-5 text-center">
             <form class="form" style="max-width: 250px; margin: auto">
                 <h1 class="h3 mb=3">LOGIN</h1>
@@ -72,14 +80,16 @@
                 <input type="email" class="form-control" id="email" placeholder="Email" required autofocus />
                 <label for="password" class="pb-3 pt-3"></label>
                 <input type="password" placeholder="Password" id="password" class="form-control" />
+
                 <!-- remember me checkbox -->
                 <div class="checkbox pb-3 pt-3">
                     <label>
                         <input type="checkbox" value="remember-me" /> Remember me
                     </label>
                 </div>
-                <div class="mt-3">
 
+                <!-- Login button -->
+                <div class="mt-3">
                     <button class="loginBtn">
                         Login
                     </button>
@@ -90,35 +100,38 @@
             </form>
         </div>
 
-        <footer class="text-center text-lg-start text-muted " style="margin-top: 150px; background-color: black;">
+        <!-- Footer -->
+        <footer class="footer text-center text-lg-start text-muted "
+            style="margin-top: 150px; background-color: black;">
+
             <!-- Section: Social media -->
             <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-                <div class="me-5 d-none d-lg-block">
-                    <span>Get connected with us on social networks:</span>
-                    <br>
-                    <br>
 
-                    <a href="https://www.instagram.com/sports4us/">
-                        <img src="images/insta.png" style="width: 30px;; height: 30px;;" alt="IG">
-
-                        <a href="https://en-gb.facebook.com/astonuniversity/">
-                            <img src="images/fb.png" style="width: 30px;; height: 30px;;" alt="fb">
-
-                            <a href="https://www.twitter.com/sports4_us">
-                                <img src="images/twitter.png" style="width: 30px;; height: 30px;;" alt="twitter">
-
-                </div>
-            </section>
-
-            <!-- Section: Links  -->
-            <section class="">
+                <!--Creating a container with row and coloumns  -->
                 <div class="container text-center text-md-start mt-5">
-                    <!-- Grid row -->
                     <div class="row mt-3">
-                        <!-- Grid column -->
                         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                             <h6 class="text-uppercase fw-bold mb-4">
-                                <p>useful links</p>
+                                <!--Links  -->
+                                <a class="fas fa-gem me-3 text-reset">Get connected with us</a>
+                            </h6>
+                            <a href="https://www.instagram.com/sports4us/">
+                                <img src="images/insta.png" style="width: 30px; height: 30px; margin-top:15px"
+                                    alt="IG"></a>
+                            <br>
+                            <a href="https://en-gb.facebook.com/astonuniversity/">
+                                <img src="images/fb.png" style="width: 30px; height: 30px; margin-top:15px"
+                                    alt="fb"></a>
+                            <br>
+                            <a href="https://www.twitter.com/sports4_us">
+                                <img src="images/twitter.png" style="width: 30px; height: 30px; margin-top:15px"
+                                    alt="twitter"></a>
+                        </div>
+
+                        <!-- Links to other pages -->
+                        <div class="socialL col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                <a class="fas fa-gem me-3 text-reset">Useful Links</a>
                             </h6>
                             <p>
                                 <a href="index.php" class="text-reset">Home</a>
@@ -133,24 +146,28 @@
                                 <a href="Contact Page.html" class="text-reset">Contact Us</a>
                             </p>
                             <p>
-                                <a href="#!" class="text-reset">About Us</a>
+                                <a href="about us.html" class="text-reset">About Us</a>
                             </p>
                         </div>
-                        <!-- Newsletter -->
                         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                            <!-- Content -->
-                            <h6 class="text-uppercase fw-bold mb-4">
-                                <a href="#" class="fas fa-gem me-3 text-reset">Subscribe Newsletter</a>
-                                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                                    <div class="subscribe btm-brdr">
-                                        <form>
-                                            <input type="email" placeholder="Email" />
-                                            <button type="button" class="fill-btn">Subscribe</button>
-                                        </form>
-                            </h6>
-                        </div>
 
+                            <!-- Newsletter -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                <a class="fas fa-gem me-3 text-reset">Subscribe Newsletter</a>
+                            </h6>
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+                                <div class="subscribe btm-brdr">
+                                    <form>
+                                        <input type="email" placeholder="Email" />
+                                        <button type="button" class="nletterBtn fill-btn">Subscribe</button>
+                                    </form>
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                </div>
             </section>
 
             <!-- Copyright -->
@@ -158,13 +175,11 @@
                 © 2022 Copyright:
                 <a class="text-reset fw-bold" href="https://mdbootstrap.com/">sports4us.com</a>
             </div>
-            <!-- Copyright -->
         </footer>
-    </body>
 
 
 </html>
-<style>
+<!-- <style>
 .form {
     background-color: white;
     padding: 20px 15px;
@@ -211,14 +226,10 @@ footer button {
 .loginBtn {
     display: block;
     margin-left: auto;
-    padding: 15px 30px;
     background-color: black;
-    color: white;
     border-radius: 6px;
     cursor: pointer;
-    font-size: 16px;
     margin-top: 30px;
-    background-color: black;
     border: black;
     color: white;
     padding: 10px 75px;
@@ -230,4 +241,18 @@ footer button {
     transform: translateY(-2px);
 }
 
-</style>
+.nletterBtn {
+    background-color: grey;
+    margin-top: 25px;
+    padding: 10px 88px;
+    font-size: 20px;
+    border-color: white;
+    text-transform: uppercase;
+}
+
+.nletterBtn:hover {
+    background-color: red;
+    transform: translateY(-2px);
+}
+
+</style> -->
