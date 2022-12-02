6933273da71a778fd85e86aa126046cf79ac3b69
Ibraheem khan <ibkhan2026@gmail.com>
2022-12-02 12:08:59 +0000

    already registered option now returns to correct form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,7 +57,7 @@
                 @endif
 
                 <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('userlogin') }}">
                         {{ __('Already registered?') }}
                     </a>
 
